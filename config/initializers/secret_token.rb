0a9afb3952e625bd5ae1708f37e8ccdb5bfbf45c
Rafael Gaspar <rafael.gaspar@gmail.com>
2010-07-02 04:58:54 -0300

    Commit Inicial - Aplicação Rails Vazia

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'c8ef4f22c472fd8b45dcd7d3971fd0a0b3f31a0953c7227f474b5e9a3b38f9f4ad8070ccb65c65ffaa39897eeab1405bf7ccdad46b6f38553cd9f56239814e14'

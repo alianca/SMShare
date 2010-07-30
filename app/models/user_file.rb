52d9cd6cb5692b47777a482c39e4962826ba9463
Rafael Gaspar <rafael.gaspar@gmail.com>
2010-07-30 02:56:25 -0300

    Adiciona os campos base de um arquivo

--- app/models/user_file.rb
+++ app/models/user_file.rb
@@ -1,3 +1,10 @@
 class UserFile
   include Mongoid::Document
+  include Mongoid::Timestamps
+  
+  # Define o esquema logico db.user_files
+  # filename, filetype, e filesize vão ser criados e matidos pelo CarrierWave
+  field :categories, :type => Array
+  field :tags, :type => Array
+  field :description, :type => String
 end

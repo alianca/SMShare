7b816a0bc3cdf6f7b27c735725a21065e8ae31c5
Rafael Gaspar <rafael.gaspar@gmail.com>
2010-07-30 02:44:31 -0300

    Pequena refatoração nos testes da autenticação

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,20 +17,20 @@ describe User do
     it "should be able to find all the user an user refered" do
       @old_user.referred.should include(@new_user)
     end
-  end  
+  end
+  
   describe "Authentication" do
     before(:each) do
       @user = User.create! :name => "Robin Sage", :nickname => "sage",
-              :email => "sage@ihaveyoursecrets.com", :password => "123456"
+          :email => "sage@ihaveyoursecrets.com", :password => "123456"
     end
     
     it "should be able to login given a valid e-mail and password" do
-      User.find_for_authentication(:email => "sage@ihaveyoursecrets.com").valid_password?("123456").should be true
+      User.find_for_authentication(:email => "sage@ihaveyoursecrets.com").should be_valid_password("123456")
     end
     
     it "should be able to login given a valid username and password" do
-      User.find_for_authentication(:email => "sage").valid_password?("123456").should be true
+      User.find_for_authentication(:email => "sage").should be_valid_password("123456")
     end
-    
   end
 end

84ed4315b3aeca4ef12e7f4b001ce6fba3b486d0
Rafael Gaspar <rafael.gaspar@gmail.com>
2010-08-05 17:47:52 -0300

    Criado funcionalidade do Cucumber para a Home

---
+++ features/pagina_inicial.feature
@@ -0,0 +1,10 @@
+# language: pt
+
+Funcionalidade: Pagina Inicial
+  Para que os usuario não fiquem perdidos ao chegar ao site
+  Como um visitante
+  Eu quero ser recebido com uma pagina inicial
+  
+  Cenário: Visitar Pagina Inicial
+    Dado que eu esteja na raiz do site
+    Então eu devo estar na pagina inicial
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -8,15 +8,11 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /the home\s?page/
+    when /raiz do site/
       '/'
-
-    # Add more mappings here.
-    # Here is an example that pulls values out of the Regexp:
-    #
-    #   when /^(.*)'s profile page$/i
-    #     user_profile_path(User.find_by_login($1))
-
+    when /pagina inicial/
+      url_for(:controller => :home, :action => :index, :only_path => true)
+      
     else
       begin
         page_name =~ /the (.*) page/

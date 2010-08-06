a8face54c8ac0838a410fc08a8cf5fcac9405975
Rafael Gaspar <rafael.gaspar@gmail.com>
2010-08-06 17:34:18 -0300

    Criada funcionalidade de Download

---
+++ features/step_definitions/download_steps.rb
@@ -0,0 +1,15 @@
+Dado /^que exista o arquivo$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Então /^eu devo baixar o arquivo$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Dado /^que não exista o arquivo$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Então /^eu devo receber um erro de arquivo não encontrado$/ do
+  pending # express the regexp above with the code you wish you had
+end

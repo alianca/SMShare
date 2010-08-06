b20fc9eba2a5e51a772fb2c823dcd4be130c650e
Rafael Gaspar <rafael.gaspar@gmail.com>
2010-08-06 18:10:15 -0300

    Implementado download de arquivos

--- features/step_definitions/download_steps.rb
+++ features/step_definitions/download_steps.rb
@@ -1,15 +1,20 @@
 Dado /^que exista o arquivo$/ do
-  pending # express the regexp above with the code you wish you had
+  @tempfile = Tempfile.new("somefile.txt")
+  @tempfile.write("Hello World!")
+  @tempfile.flush
+  
+  @file = UserFile.create!(:file => @tempfile)
 end
 
 Então /^eu devo baixar o arquivo$/ do
-  pending # express the regexp above with the code you wish you had
+  page.body.should == "Hello World!"
 end
 
 Dado /^que não exista o arquivo$/ do
-  pending # express the regexp above with the code you wish you had
+  @file = UserFile.new # Cria o arquivo mas nunca salva para ele não existir
 end
 
 Então /^eu devo receber um erro de arquivo não encontrado$/ do
-  pending # express the regexp above with the code you wish you had
+  page.driver.response.status.should == 404
+  page.driver.response.content_type.should == "text/html"
 end

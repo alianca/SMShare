7615ecca54dd1f1285193c912f0d8b0d27b34c22
Rafael Gaspar <rafael.gaspar@gmail.com>
2010-08-10 18:09:53 -0300

    Implementado upload

--- app/controllers/user_files_controller.rb
+++ app/controllers/user_files_controller.rb
@@ -1,2 +1,11 @@
 class UserFilesController < ApplicationController
+  respond_to :html
+  
+  def new
+    respond_with(@file = UserFile.new)
+  end
+  
+  def create
+    respond_with(@file = current_user.files.create(params[:user_file]))
+  end
 end

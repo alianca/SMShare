dfc9351b58710a67e4e7bdc012448bf0c1c6d76e
Rafael Gaspar <rafael.gaspar@gmail.com>
2010-08-11 16:48:40 -0300

    Criado fabrica de usuarios

---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,8 @@
+Factory.define :user, :class => User do |u|
+  u.name "Sage Darkfire"
+  u.email "sage.darkfire@example.com"
+  u.nickname "sage"
+  u.password "123456"
+  u.password_confirmation "123456"
+  u.accepted_terms true
+end

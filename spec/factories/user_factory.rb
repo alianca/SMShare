3d0952fa41f654993392bb1d2652c54cce818296
Rafael Gaspar <rafael.gaspar@gmail.com>
2010-08-13 17:20:26 -0300

    Pequena correção na fabrica de usuarios

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,7 +1,7 @@
 Factory.define :user do |u|
   u.name "Sage Darkfire"
   u.email "sage.darkfire@example.com"
-  u.nickname "sage"
+  u.nickname { |u| u.name.downcase.gsub(" ", ".") }
   u.password "123456"
   u.password_confirmation "123456"
   u.accepted_terms true

a3ce0a624ae7a7f6e9fa3c277346c9eb41298dde
Rafael Gaspar <rafael.gaspar@gmail.com>
2010-08-18 17:36:45 -0300

    Refatoração no action de upload

--- app/controllers/user_files_controller.rb
+++ app/controllers/user_files_controller.rb
@@ -2,20 +2,16 @@ class UserFilesController < ApplicationController
   respond_to :html
   before_filter :authenticate_user!, :only => [:new, :create]
   
-  layout 'user_panel', :only => [:new]
+  layout 'user_panel', :only => [:new, :create]
   
   def new
     respond_with(@file = UserFile.new)
   end
   
-  def create    
-    respond_with(@file = current_user.files.create(params[:user_file])) do |format|
-      if @file.valid?
-        flash[:notice] = "Arquivo enviado com sucesso."
-      else
-        flash[:alert] = @file.errors.full_messages.first
-        format.html { render :action => :new, :layout => 'user_panel' }
-      end
-    end
+  def create
+    @file = current_user.files.create(params[:user_file])
+    flash[:notice] = "Arquivo enviado com sucesso." if @file.valid?
+    flash[:alert] = @file.errors.full_messages.first unless @file.valid?    
+    respond_with(@file)
   end
 end

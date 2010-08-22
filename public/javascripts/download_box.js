50499e3f641561bac7800504cd578708b484410b
Rafael Gaspar <rafael.gaspar@gmail.com>
2010-08-22 20:00:57 -0300

    Fecha a caixa se clicar de novo e após baixar

--- public/javascripts/download_box.js
+++ public/javascripts/download_box.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
   $("a[rel~=\"smshare\"]").click(function (sender) {
-    /* Fecha as caixas abertas */
-    $(".download_box").remove();
-    
     /* Pega os dados do link */
     link = $(sender.target);
-    user_file_url = link.attr("href").match(/arquivos\/([0-9a-f]{24})\/?$/)[1];
+    user_file_id = link.attr("href").match(/arquivos\/([0-9a-f]{24})\/?$/)[1];
+    
+    /* Verifica se já está aberto para poder fechar */
+    if($("#download_box-" + user_file_id)[0]) {
+      $("#download_box-" + user_file_id).remove();      
+      
+      return false;
+    }
     
     /* Cria a caixa de download via AJAX */
     $.ajax({
@@ -35,6 +39,15 @@ $(document).ready(function() {
     $(".clear-on-focus").blur(function() {
       if($(this).val() == "")
         $(this).val($(this).attr("title"));
+    });
+    
+    /* Fecha após o download do arquivo */
+    $("#download_box-" + user_file_id + " form").submit(function () {
+      $("#download_box-" + user_file_id).hide();
+      setTimeout(function () {
+        $("#download_box-" + user_file_id).remove();
+      }, 200);       
+      return true;
     });
         
     /* Retorna falso para o link não ser seguido caso tudo tenha dado certo */

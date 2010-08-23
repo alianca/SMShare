e0f2f0106c7c676ed95393d4978b0ae3846d8a8c
Rafael Gaspar <rafael.gaspar@gmail.com>
2010-08-23 13:45:21 -0300

    Arruma o upload no Firefox

--- public/javascripts/user_files.js
+++ public/javascripts/user_files.js
@@ -7,7 +7,14 @@ $(document).ready(function() {
   /* Volta o fundo padrão quando perde o mouse over */
   $("#new_user_file #user_file_submit").mouseout(function () {
     $(this).css("background", "url(/images/user_files/botao-off.png)")
-  });  
+  });
+  
+  /* Arruma o file_field no Firefox */
+  if($.browser.mozilla) {
+      $("#new_user_file #user_file_file").attr("size", 46);
+      $("#new_user_file #user_file_file_input").css("padding", "7px 8px");
+      $("#new_user_file #user_file_file_input").css("background", "url(/images/user_files/campo.png)");
+  }
 });
 
 /* Faz pre-cache das imagens do cadastro */

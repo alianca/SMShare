265057beeb46b20910c5aed27ee890ad48cd7624
Rafael Gaspar <rafael.gaspar@gmail.com>
2010-08-23 13:57:17 -0300

    Instalado Capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,53 @@
+# Application
+set :application, "smshare"
+set :deploy_to, "/var/rails/#{application}"
+
+# Repository
+set :scm, :git
+set :repository,  "git@github.com:rafaelgaspar/SMShare.git"
+
+# Run Options
+set :user, "root"
+set :use_sudo, false
+default_run_options[:pty] = true
+
+# Servers
+role :web, "sparkledew.vidanerd.com"
+role :app, "sparkledew.vidanerd.com"
+role :db,  "sparkledew.vidanerd.com", :primary => true
+
+#	Passenger Recipes
+namespace :deploy do
+  task :start do; end
+  task :stop do; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+# Bundler Recipes
+namespace :bundler do
+  task :create_symlink, :roles => :app do
+    run("mkdir -p #{File.join(shared_path, 'bundle')} && ln -s #{File.join(shared_path, 'bundle')} #{File.join(current_release, '.bundle')}")
+  end
+  
+  task :install, :roles => :app do
+    bundler.create_symlink
+    run "cd #{current_path} && bundle install --deployment"
+  end
+end
+after "deploy:symlink", "bundler:install"
+
+# MongoDB Recipes
+namespace :deploy do
+  task :migrate, :only => {:primary => true}, :roles => :db do; end # Ignore Migrations
+end
+
+# NginX Recipes
+namespace :nginx do
+  desc "Change owner to www-data" 
+  task :fix_owner, :roles => [ :app, :db, :web ] do
+          run "#{try_sudo} chown -R www-data:www-data #{deploy_to}" 
+  end
+end
+after "deploy:symlink", "nginx:fix_owner"

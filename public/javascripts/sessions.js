7000f57635657b5ffe9c9d93c90627971fc8c5df
Rafael Gaspar <rafael.gaspar@gmail.com>
2010-09-23 22:57:33 -0300

    Estilizado o Layout do Login

---
+++ public/javascripts/sessions.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  /* Troca o fundo do botão em mouse over */
+  $("#user_new #user_submit").mouseover(function () {
+    $(this).css("background", "url(/images/layouts/botao-on.png)")
+  });
+
+  /* Volta o fundo padrão quando perde o mouse over */
+  $("#user_new #user_submit").mouseout(function () {
+    $(this).css("background", "url(/images/layouts/botao-off.png)")
+  });
+});
+
+/* Faz pre-cache das imagens do cadastro */
+$.cacheImages("/images/layouts/botao-on.png");

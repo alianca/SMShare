41837fec0e8c3c90bca069a2a75d7dc6ab5ee18c
Rafael Gaspar <rafael.gaspar@gmail.com>
2010-12-10 23:59:34 -0200

    Arruma carrierwave para funcionar com o novo mongoid

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,4 @@
 CarrierWave.configure do |config|
   config.grid_fs_database = Mongoid.database.name
-  config.grid_fs_host = Mongoid.config.master.connection.host
+  config.grid_fs_host = Mongoid.database.connection.primary_pool.host
 end

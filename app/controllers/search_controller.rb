15986238e48aaf08c254bd5923db2b9bdb189b57
Rafael Gaspar <rafael.gaspar@gmail.com>
2011-03-30 12:24:39 -0300

    Frontend da Busca

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,5 @@
 class SearchController < ApplicationController
   def index
-    @files = UserFile.search(params[:q]).paginate(:per_page => 10, :page => params[:p]) if params[:q]
+    @files = UserFile.search(params[:q]).paginate(:per_page => 10, :page => params[:page]) if params[:q]
   end  
 end

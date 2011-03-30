2e192a41609c2c76bcc7f30ab636d1c7494e01cc
Fernando Cezar 'China <netcriptus@gmail.com>
2011-03-30 16:25:56 -0300

    Correções pequenas de sintaxe e português

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
+  
+  private
+    def require_admin!
+      authenticate_user!
+      redirect_to new_user_session unless current_user.is_admin?
+    end
+  
 end

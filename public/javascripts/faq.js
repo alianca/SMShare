1b7abfe1e76555bee46ce91290f9e9a941de1839
666.felipe@gmail.com <edric@betelgeuse.(none)>
2011-04-05 20:18:02 -0300

    Adicionado o efeito de hover nas abas e animação ao abrir os itens do FAQ

--- public/javascripts/faq.js
+++ public/javascripts/faq.js
@@ -15,11 +15,11 @@ $(document).ready(function() {
       
     if (item.hasClass("open")) {
       question.css("background", "url(/images/faq/fundo_seta_baixo.png) no-repeat");
-      answer.hide();
+      answer.hide("fast");
       item.removeClass("open").addClass("closed");
     } else if (item.hasClass("closed")) {
       question.css("background", "url(/images/faq/fundo_seta_cima.png) no-repeat");
-      answer.show();
+      answer.show("fast");
       item.removeClass("closed").addClass("open");
     }
     

aecc734edf371d26cc71a970cd93c8000945b3ff
Felipe Trzaskowski <666.felipe@gmail.com>
2011-08-08 18:29:57 -0300

    Filtro das referências

--- app/models/user_reference.rb
+++ app/models/user_reference.rb
@@ -1,17 +1,18 @@
 class UserReference
   include Mongoid::Document
-  
+  include Mongoid::Timestamps
+
   field :name, :type => String
   field :clicks, :type => Integer, :default => 0
-  
+
   embedded_in :user, :inverse_of => :reference
-  
+
   validates :name, :presence => true
-  
+
   def signups
     user.referred.where(:referred_by => self.name).count
   end
-  
+
   def comission
     user.referred.where(:referred_by => self.name).collect(&:'statistics.referrer_comission').sum
   end

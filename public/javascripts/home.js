b050fdbad9f6d1eb3744547417a807fbdba47f2b
Felipe Trzaskowski <666.felipe@gmail.com>
2012-03-29 13:43:55 -0300

    Autoplay nos flashes

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,16 +1,25 @@
+function setAnimation(i) {
+  /* Menu */
+  $("#tv-menu li").removeClass("active");
+  $("#tv-menu li:nth(" + i + ")").addClass("active");
+  /* Slide */
+  $(".slide").hide();
+  $(".slide:nth(" + i + ")").show();
+
+  setTimeout(function() {
+    setAnimation((i + 1) % $("#tv-menu li").length);
+  }, 6000);
+}
+
 $(document).ready(function() {
-  /* TV */
   $("#tv-menu li").each(function (i, element) {
-    $(element).click(function (sender) {
-      /* Menu */
-      $("#tv-menu li").removeClass("active");
-      $("#tv-menu li:nth(" + i + ")").addClass("active");
-      /* Slide */
-      $(".slide").hide();
-      $(".slide:nth(" + i + ")").show();
+    $(element).click(function(sender) {
+      setAnimation(i);
     });
   });
+
+  setAnimation(0);
 });
 
 /* Faz pre-cache das imagens da pagina inicial */
-$.cacheImages("/images/layouts/botao-on.png");
+cache_images("/images/layouts/botao-on.png");

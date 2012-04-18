1b4ff20f237b3a84047241cf30b69439c7c275f1
Edric Garran <666.felipe@gmail.com>
2012-04-18 13:20:38 -0300

    Arrumado o bug de poder mexer nas imagens sem estar autenticado

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
   prepend_view_path "app/views/devise"
-
   layout :choose_layout
 
   def edit
@@ -9,30 +8,6 @@ class RegistrationsController < Devise::RegistrationsController
       @states = Carmen::states(@user.profile.country)
     rescue
       @states = []
-    end
-  end
-
-  def create
-    resource.referred_by = cookies[:referred_banner] if cookies[:referred_banner]
-
-    if resource.save
-      if cookies[:referred_user]
-        @referred_user = User.where(:nickname => cookies[:referred_user]).first
-        @referred_user.referred << resource
-      end
-
-      if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_navigational_format?
-        sign_in(resource_name, resource)
-        respond_with resource, :location => after_sign_up_path_for(resource)
-      else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
-        expire_session_data_after_sign_in!
-        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
-      end
-    else
-      clean_up_passwords resource
-      respond_with resource
     end
   end
 
--- app/uploaders/user_file_image_uploader.rb
+++ app/uploaders/user_file_image_uploader.rb
@@ -1,6 +1,10 @@
 class UserFileImageUploader < CarrierWave::Uploader::Base
   storage :grid_fs
 
+  def extension_white_list
+    %w(jpg jpeg gif png bmp)
+  end
+
   def store_dir
     "user_file_image/#{model.id}"
   end

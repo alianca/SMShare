e66d3c37e06cd54aa9fa4da8015ddac6773140eb
Edric Garran <666.felipe@gmail.com>
2012-07-27 01:40:58 -0300

    user_id

--- public/javascripts/user_panel/uploads.js
+++ public/javascripts/user_panel/uploads.js
@@ -75,7 +75,7 @@ $(document).ready(function() {
   function update_status(form) {
     var status;
     $.ajax({
-      url: "http://69.64.50.217/progress?X-Progress-ID=" + $(form).find(".file_fields input[type=hidden]").val(),
+      url: "http://69.64.50.217/progress?X-Progress-ID=" + $(form).find(".file_fields #X-Progress-ID").val(),
       dataType: "json",
       async: false,
       error: function(e) { status = 'error' },
@@ -180,7 +180,7 @@ $(document).ready(function() {
 
 
   /* Salva o id do upload */
-  var upload_id = $('#new_user_file .file_fields input[type=hidden]').val();
+  var upload_id = $('#new_user_file .file_fields #X-Progress-ID').val();
   var upload_action = $('#new_user_file').attr('action');
 
 
@@ -189,7 +189,7 @@ $(document).ready(function() {
   $('#new_user_file_0').attr('target', 'new_user_file_iframe_0');
   $('#new_user_file_0').attr('action', upload_action + '-0');
   $('#new_user_file_0').append('<iframe name="new_user_file_iframe_0"></iframe>');
-  $('#new_user_file_0 .file_fields input[type=hidden]').val(upload_id + '-0');
+  $('#new_user_file_0 .file_fields #X-Progress-ID').val(upload_id + '-0');
   var file_form_template = $('#new_user_file_0').clone();
   make_file_field($('#new_user_file_0 .file_fields .file input[type=file]'));
   make_checkbox($('#new_user_file_0 input[type=checkbox]'));
@@ -203,7 +203,7 @@ $(document).ready(function() {
     $(new_form).attr('target', 'new_user_file_iframe_' + form_count);
     $(new_form).attr('action', upload_action + '-' + form_count);
     $(new_form).children('iframe').attr('name', 'new_user_file_iframe_' + form_count);
-    $(new_form).find('.file_fields input[type=hidden]').val(upload_id + '-' + form_count);
+    $(new_form).find('.file_fields #X-Progress-ID').val(upload_id + '-' + form_count);
     new_form[0].reset();
     $(new_form).find('.clear-on-focus').val($(new_form).find('.clear-on-focus').attr('title'));
     $(new_form).find('.public_field input[type=checkbox]').attr('id', 'user_file_' + form_count + '_public');

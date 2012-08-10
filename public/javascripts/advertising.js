1d62f380a579ca75471f594612fd9e2d37455247
Edric Garran <666.felipe@gmail.com>
2012-08-10 18:00:19 -0300

    peças publicitarias adicionadas, faltam os links

---
+++ public/javascripts/advertising.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+  $('#boxes .box, #boxes .links-box').hide();
+  $('#boxes .preview').hide();
+
+  $('#buttons div.button').click(function(e) {
+    $('#buttons .button.active').removeClass('active');
+    $(this).addClass('active');
+
+    $('#boxes .box:visible').hide();
+    $('#boxes .' + $(this).prop('id')).fadeIn('fast')
+  });
+
+  $('#buttons #horizontal').click();
+
+
+  $('#boxes .box').each(function() {
+    var box = $(this);
+
+    box.find('.mini').click(function(e) {
+      box.find('.mini.active').removeClass('active');
+      $(this).addClass('active');
+
+      box.find('.preview:visible').hide();
+      box.find('.preview.' + $(this).text()).fadeIn('fast');
+    });
+  });
+
+  $('#boxes .box .mini.active').click();
+
+});

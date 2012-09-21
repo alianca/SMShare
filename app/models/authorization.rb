1f2136571808f5b68f0256bf887325129bb49212
Felipe Trzaskowski <666.felipe@gmail.com>
2012-09-21 17:09:26 -0300

    identação

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -11,7 +11,7 @@ class Authorization < RedisModel
 
   def self.register params
     params.each { |k, v|
-      logger.info "PARAM[#{k}] => #{v}"
+      puts "PARAM[#{k}] => #{v}"
     }
     return "0" if params[:pin].blank?
     auth = self.new params[:pin], {
@@ -43,7 +43,7 @@ class Authorization < RedisModel
 
   def check
     url = confirm_url
-    logger.info "Sending MT: #{url}"
+    puts "Sending MT: #{url}"
     if Curl::Easy.perform(confirm_url).body_str != "0"
       self.destroy
       return "0"
